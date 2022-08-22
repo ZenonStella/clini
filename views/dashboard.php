776fe6865d432ceef1d4632e87712fed6a60fa86
Eosiin <jordan.mabille76@outlook.fr>
2022-08-22 16:13:26 +0200

    dashbord view css

---
+++ views/dashboard.php
@@ -0,0 +1,22 @@
+<?php
+include('templates/header.php');
+?>
+<div class="p-3">
+    <p class="h2 text-center">Bienvenue</p>
+    <div class="row justify-content-evenly text-light py-5">
+        <div class="col-lg-5 col-10 text-center">
+            <div class="row justify-content-center">
+                <a class="col-lg-5 col-8 my-3 text-center p-3 mx-3 btn btn-danger text-decoration-none text-light" href="">Ajouter patient</a>
+                <a href="" class="text-decoration-none text-light col-lg-5 col-8 my-3 text-center p-3 mx-3 btn btn-danger">Gestion des patient</a>
+                <a href="" class="text-decoration-none text-light col-lg-5 col-8 my-3 text-center p-3 mx-3 btn btn-danger">Ajouter rendez vous</a>
+                <a href="" class="text-decoration-none text-light col-lg-5 col-8 my-3 text-center p-3 mx-3 btn btn-danger">Gestion des rendez vous</a>
+                <a href="" class="text-decoration-none text-light col-lg-5 col-8 my-3 text-center p-3 mx-3 btn btn-danger">Ajouter spécialiste</a>
+                <a href="" class="text-decoration-none text-light col-lg-5 col-8 my-3 text-center p-3 mx-3 btn btn-danger">Gestion des spécialistes</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include('templates/footer.php');
+?>
